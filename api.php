81def32efb22062364f7d15e82944312de33527c
ariestahrt <ariestaheart@gmail.com>
2020-11-18 18:29:10 +0700

    Table rows using parsing JSON from ajax

--- api.php
+++ api.php
@@ -1,4 +1,9 @@
 <?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Methods: *");
+
 include_once('simple_html_dom.php');
 function getStr($string, $start, $end) {
     $str = explode($start, $string);
